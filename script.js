c5323625b0acdafb72929429d2e22018a3995249
Jon Keopangna <jkeopangna@gmail.com>
2020-10-18 01:15:01 -0600

    Added CSS, started quiz function

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
-///////TITLE PAGE///////
+var section = document.getElementById('section')
 
-// Variable of title for h1 element
-var title = document.createElement("h1");
-title.innerHTML = "Javascript Quiz";
-document.body.appendChild(title);
-title.setAttribute("style", "color: navy");
-title.style.textAlign = "center";
-title.style.fontFamily = "comic sans ms";
-title.style.marginTop = "10rem"
+var startBtn = document.getElementById('startBtn')
 
-// Variable for p element under title
-var p = document.createElement("h2");
-p.innerHTML = "Challenge yourself to a timed quiz about Javascript. But watch the clock! Time will be subtracted when any questions are answered incorrectly. Check out the high scores after you finish!"
-document.body.appendChild(p);
-p.style.fontSize = "18px"
-p.style.textAlign = "center"
+var questionContainer = document.getElementById('question-container')
+
+var showQuestion = document.getElementById('question')
+
+var showAnswers = document.getElementById('btn')
+
+var answers = ["5", "Michael Myers", "Wayan Bros", "The upside down", "3"]
+
+var questions = ["How many Scream movies have been made?", "Who is the man behind the mask in the Halloween movies?", "Which pair of brothers directed Scary Movie?", "Where did the Mind Flayer come from?", "How many times must you say Beetlejuice?"]
+
+
+
+startBtn.addEventListener('click', startGame)
+showAnswers.addEventListener('click', function (){
+    questions++
+    question()
+})
+
+function startGame() {
+    console.log('Start');
+    section.classList.add('hide');
+    questionContainer.classList.remove('hide')
+    Question()
+}
+
+function nextQuestion(){
+    
+}
+
+function Question(){
+    showQuestion.innerText = questions[1]
+    for (var i = 0; i < answers1; i++) {
+        showAnswers.innerText = answers1.Math.floor(Math.random())
+    }
+    }
+
+
