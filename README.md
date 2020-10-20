10f0436636e933070a53a04e66647994225557fd
Jon Keopangna <jkeopangna@gmail.com>
2020-10-19 22:35:36 -0600

    Updated all files

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # codeQuiz
-HW4 
+
+HW4: Halloween Code Quiz
+
+The purpose of this quiz application provides a better understanding of what a future full-stack web developer can experience during an interview process. The process would involve a coding assessment which can consist a combination of multiple-choice questions and interactive coding challenges.
+
+To familiarize oneself with these types of processes, a timed coding quiz with multiple choice questions is developed. The skills obtained thus far are applied to the application creating HTML, CSS and Javascript elements. In return, the quiz performs the following:
+
+    1. Start button to begin the quiz and start the timer
+    2. Multiple choice questions that if answered incorrectly, deducts time from the timer countdown
+    3. Submit and track scores using local storage
+
+Links are provided below to preview the finished application.
--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 /////////VARIABLES/////////
 
-// var section = document.getElementById('section')
 
 // Start button to start quiz
 
 var startBtn = document.getElementById
 ('startBtn')
 
+// Play again button to return to home screen
+
+var playAgain = document.getElementById('playAgain')
+
 // Container element that holds quiz questions
 
 var questionContainer = document.getElementById('question-container')
 
+// Container element that holds score submittions
+
+var scoreContainer = document.getElementById('score-container')
+
 // Variables set for timer function
 
-var timer = 1;
-let time = timer * 60;
-var countdown = document.getElementById('timer');
+var startTime = 1
+let time = 60;
+var endTime = 0;
+var countdown = document.getElementById('startTime');
+
+// Variables to store & track scores
+
+var username = document.getElementById('username')
+var saveScore = document.getElementById('saveScore')
+var finalScore = document.getElementById('finalScore')
+var highScores = localStorage.getItem('highScores')
 
 // Variable to document questions on quiz
 
@@ -23,20 +38,43 @@ var showQuestion = document.getElementById('question')
 
 // Variable to document answers on answer buttons to the HTML
 
-var showAnswers = document.getElementById('btn')
-
-var answers = ["5", "Michael Myers", "Wayan Bros", "The upside down", "3"]
+var showAnswers = document.getElementById('choiceBtn')
 
 // Variable with an array that holds the quiz questions
 
-var questions = ["How many Scream movies have been made?", "Who is the man behind the mask in the Halloween movies?", "How many times must you shouldn't say Beetlejuice?"]
+var questions = [
+    { quizQuestions: "How many Scream movies have been made?",
+      choices: [
+          {text: "5", correct: true}, 
+          {text: "3", correct: false},
+          {text: "8", correct: false},
+          {text: "2", correct: false},
+        ]
+    
+},
+    { quizQuestions: "Who is the man behind the mask in the Halloween movies?",
+    choices: ["Freddie, Michael Myers, Ben Macchok, Jeff"],
+    answer: "Michael Myers"
+},
+    { quizQuestions: "How many times must you shouldn't say Beetlejuice?",
+      choices: ["7", "1", "50", "3"],
+      answer: "3"
+}    
+]
 
-var answers1 = [5, 3, 8, 2]
+// Local storage element to track scores
+
+localStorage.setItem('highScores', [])
+console.log(localStorage.getItem('highScores'))
+
+// username.addEventListener('keyup', () => )
 
 /////////Event Listeners//////////
 
 // Event listener that starts quiz on click
 startBtn.addEventListener('click', startQuiz)
+
+// playAgain.addEventListener('click')
 
 // Timer interval by seconds starting at 1min
 setInterval(startTimer, 1000);
@@ -52,14 +90,15 @@ showAnswers.addEventListener('click', function (){
 
 // Timer function
 function startTimer() {
-    var minutes = 0;
-    let seconds = time % 1000;
-    // seconds = seconds < 10 ? '0' + seconds : seconds;
-    countdown.innerHTML = minutes + seconds
+  
+    var seconds = time % 1000;
+    seconds = seconds < 1 ? '0' + seconds : seconds;
+    countdown.innerHTML = seconds
     time--;
     time = time < 0 ? 0 : time;
-    if (sountdown == 0 * 1000) {
-        startQuiz()
+    if (time === 00) {
+        questionContainer.classList.add('hide')
+        scoreContainer.classList.remove('hide')
     }
 }
 
@@ -74,19 +113,21 @@ function startQuiz() {
     
 
 function Question(){
-    showQuestion.innerHTML = questions[0]
-    // for (var i = 0; i < questions.length; i++) {
-    //    showQuestion.innerHTML = Math.floor(Math.random() * questions.length)
-    // }
-    // for (var i = 0; i < answers1.length; i++) {
-    //     showAnswers.innerText = answers1
-    // }
-    Question()
+
+    var test1 = questions[0]
+
+    var test = document.getElementById('question')
+
+    test.innerHTML = test1.quizQuestions
+
+    questions.choices.forEach(choices => {
+        var button = document.getElementById('button')
+        button.innerHTML = choices
+    })
     }
 
     
-function nextQuestion(){
-    showQuestion.innerHTML = questions[1]
+function playAgain(){
+    questionContainer.classList.add('hide');
+    score-container.classList.remove('hide');
 }
-
-
